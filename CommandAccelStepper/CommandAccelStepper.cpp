e51483d8caab70753c2c1a0e4518668628785375
ShinRa26 <grahamkeenan@hotmail.co.uk>
2016-10-24 15:08:13 +0100

    Added CommandAccelStepper

--- CommandAccelStepper/CommandAccelStepper.cpp
+++ CommandAccelStepper/CommandAccelStepper.cpp
@@ -1,172 +1,427 @@
-
 #include "CommandAccelStepper.h"
 
-void* globalCommandAccelStepperPt2Object;
+void* globalCommandLinearAccelStepperActuatorPt2Object;
 
-CommandAccelStepper::CommandAccelStepper(uint8_t interface, uint8_t pin1, uint8_t pin2, uint8_t pin3, uint8_t pin4, bool enable) {
-
-  AccelStepper mystepper(interface, pin1,pin2, pin3, pin4, enable);
-  stepper = mystepper;
+CommandAccelStepper::CommandAccelStepper(AccelStepper &myStepper, int myEnablePin)
+{
+    LinearAccelStepperActuator my_linearactuator = LinearAccelStepperActuator(myStepper, myEnablePin);
+    linearactuator = my_linearactuator;
 }
 
 /**
-* register to a CommandManager
+* Register to Command Manager
 */
-void CommandAccelStepper::registerToCommandManager(CommandManager &cmdMng, const char *command) {
-  cmdMng.addDevice(command, this, wrapper_init, wrapper_handleCommand, wrapper_setHeader, wrapper_update);
-}
-
-
-/**
-* init function to be run one in setup
-* we initialize all the callback here
-*/
-void CommandAccelStepper::wrapper_init(void* pt2Object) {
-  // explicitly cast to a pointer to Classname
-  CommandAccelStepper* self = (CommandAccelStepper*) pt2Object;
-  self->init();
-}
-
-void CommandAccelStepper::init() {
-  #ifdef COMMANDACCELSTEPPER_DEBUG
-    Serial.println("Init CommandStepper");
-  #endif
-
-  stepper.setMaxSpeed(5000);
-  stepper.setAcceleration(2000);
-
-  // here register all the commands
-  // the following is mandatory for the bonjour behavior
-  cmdHdl.addCommand("BONJOUR", wrapper_bonjour);
-  cmdHdl.addCommand(COMMANDACCELSTEPPER_MOVE_TO, wrapper_moveTo);
-
-  // the default unrecognized, keep it
-  cmdHdl.setDefaultHandler(wrapper_unrecognized);
+void CommandAccelStepper::registerToCommandManager(CommandManager &cmdMgr, const char *command)
+{
+    cmdMgr.addDevice(command, this, wrapper_init, wrapper_handleCommand, wrapper_setHeader, wrapper_update);
 }
 
 /**
-* message are redicted here
+* Init function to be run once in setup.
+* We initialise all the callback here
+* Wrapper is used by CommandManager
 */
-void CommandAccelStepper::wrapper_handleCommand(const char *command, void* pt2Object) {
-  // each time the handleCommand is called, it is given the command and the pointer to the instance that should handle it
-  globalCommandAccelStepperPt2Object = pt2Object;
-  // explicitly cast to a pointer to Classname
-  CommandAccelStepper* self = (CommandAccelStepper*) globalCommandAccelStepperPt2Object;
-
-  self->handleCommand(command);
+void CommandAccelStepper::wrapper_init(void* pt2Object)
+{
+    CommandAccelStepper* self = (CommandAccelStepper*) pt2Object;
+    self->init();
 }
 
-void CommandAccelStepper::handleCommand(const char *command) {
-  #ifdef COMMANDACCELSTEPPER_DEBUG
-    Serial.print("Stepper received: ");
-    Serial.println(command);
-  #endif
+void CommandAccelStepper::init()
+{
+    #ifndef CommandAccelStepper_DEBUG
+        Serial.println("Init CommandAccelStepper");
+    #endif
 
-  cmdHdl.processString(command);
+    //do device init first
+    linearactuator.init();
+
+    //Register all commands.
+    //ALL OF THEM
+    cmdHdl.addCommand(BONJOUR_CMD, wrapper_bonjour);
+
+    cmdHdl.addCommand(COMMANDACCELSTEPPER_SET_POSITION, wrapper_setCurrentPosition);
+    cmdHdl.addCommand(COMMANDACCELSTEPPER_SET_SPEED, wrapper_setSpeed);
+    cmdHdl.addCommand(COMMANDACCELSTEPPER_SET_MAX_SPEED, wrapper_setMaxSpeed);
+    cmdHdl.addCommand(COMMANDACCELSTEPPER_SET_ACC, wrapper_setAcceleration);
+
+    cmdHdl.addCommand(COMMANDACCELSTEPPER_ENABLE_ACC, wrapper_enableAcceleration);
+    cmdHdl.addCommand(COMMANDACCELSTEPPER_DISABLE_ACC, wrapper_disableAcceleration);
+
+    cmdHdl.addCommand(COMMANDACCELSTEPPER_MOVE_TO, wrapper_moveTo);
+    cmdHdl.addCommand(COMMANDACCELSTEPPER_MOVE, wrapper_move);
+    cmdHdl.addCommand(COMMANDACCELSTEPPER_STOP, wrapper_stop);
+
+    cmdHdl.addCommand(COMMANDACCELSTEPPER_MOVING, wrapper_isMoving);
+    cmdHdl.addCommand(COMMANDACCELSTEPPER_REQUEST_DIST, wrapper_distanceToGo);
+    cmdHdl.addCommand(COMMANDACCELSTEPPER_TARGET, wrapper_targetPosition);
+    cmdHdl.addCommand(COMMANDACCELSTEPPER_REQUEST_POSITION, wrapper_currentPosition);
+
+    cmdHdl.addCommand(COMMANDACCELSTEPPER_REQUEST_SPEED, wrapper_speed);
+    cmdHdl.addCommand(COMMANDACCELSTEPPER_REQUEST_MAXSPEED, wrapper_maxSpeed);
+    cmdHdl.addCommand(COMMANDACCELSTEPPER_REQUEST_ACCELERATION, wrapper_acceleration);
+
+    //Default one
+    cmdHdl.setDefaultHandler(wrapper_unrecognized);
 }
 
 /**
-* set Header for the message that come out of this guy
+* Handling messages
+* wrapper is used by CommandManager
 */
-void CommandAccelStepper::wrapper_setHeader(const char *cmdHeader, void* pt2Object) {
-  // explicitly cast to a pointer to Classname
-  CommandAccelStepper* self = (CommandAccelStepper*) pt2Object;
-  self->setHeader(cmdHeader);
+void CommandAccelStepper::wrapper_handleCommand(const char *command, void* pt2Object)
+{
+    //Each time the handleCommand is called, it is given the command and the pointer to the instance that should handle it
+    globalCommandLinearAccelStepperActuatorPt2Object = pt2Object;
+    //Cast it to class name
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+
+    self->handleCommand(command);
 }
 
-void CommandAccelStepper::setHeader(const char *cmdHeader) {
-  #ifdef COMMANDACCELSTEPPER_DEBUG
-    Serial.println("Init CommandStepper");
-  #endif
+void CommandAccelStepper::handleCommand(const char *command)
+{
+    #ifndef CommandAccelStepper_DEBUG
+        Serial.print("Device received: ");
+        Serial.println(command);
+    #endif
 
-  cmdHdl.setCmdHeader(cmdHeader);
+    cmdHdl.processString(command);
 }
 
 /**
-* update function to be run each loop
+* Set Header for the message that comes out of the Device
+* The wrapper is used by the CommandManager
 */
-void CommandAccelStepper::wrapper_update(void* pt2Object) {
-  // explicitly cast to a pointer to Classname
-  CommandAccelStepper* self = (CommandAccelStepper*) pt2Object;
-  self->update();
+void CommandAccelStepper::wrapper_setHeader(const char *cmdHeader, void* pt2Object)
+{
+    //Explicitly cast to a pointer to classname
+    CommandAccelStepper* self = (CommandAccelStepper*) pt2Object;
+    self->setHeader(cmdHeader);
 }
 
-void CommandAccelStepper::update() {
-  stepper.run();
+void CommandAccelStepper::setHeader(const char *cmdHeader)
+{
+    #ifndef CommandAccelStepper_DEBUG
+        Serial.print("Set Header CommandAccelStepper to ");
+        Serial.println(cmdHeader);
+    #endif
+
+    cmdHdl.setCmdHeader(cmdHeader);
 }
 
-/*
-* a bonjour behavior enable to know who we are talking to
-* change the COMMANDACCELSTEPPER_BONJOUR_ID and ensure it is unique to your new device
-* keep COMMANDACCELSTEPPER_BONJOUR_ID short
-* do not forget to change the name ofthis variable apropriately
+/**
+* Update function to be run each loop
+* The wrapper is used by the CommandManager
+*/
+void CommandAccelStepper::wrapper_update(void* pt2Object)
+{
+    //Cast to pointer of classname
+    CommandAccelStepper* self = (CommandAccelStepper*) pt2Object;
+    self->update();
+}
+
+void CommandAccelStepper::update()
+{
+    //Do what needs to be done. Non-blocking things
+    //Should be fast!!
+    linearactuator.update();
+}
+
+/**
+* A Bonjour behaviour eenable to know who we are talking to
+* change the CommandAccelStepper_BONJOUR_ID  and ensure it is unique to your new device
+* keep CommandAccelStepper_BONJOUR_ID short
+* DONT FORGET TO CHANGE THE NAME OF THIS VARIABLE APPROPRIATELY!!
 */
 void CommandAccelStepper::wrapper_bonjour()
 {
-  // explicitly cast to a pointer to Classname
-  CommandAccelStepper* self = (CommandAccelStepper*) globalCommandAccelStepperPt2Object;
-  self->bonjour();
+    //Cast once more...
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->bonjour();
 }
 
-void CommandAccelStepper::bonjour() {
-  #ifdef COMMANDACCELSTEPPER_DEBUG
-    Serial.println("Device received bonjour command");
-  #endif
+void CommandAccelStepper::bonjour()
+{
+    #ifndef CommandAccelStepper_DEBUG
+        Serial.println("Device received bonjour command");
+    #endif
 
-  cmdHdl.initCmd();
-  cmdHdl.addCmdString("BONJOUR");
-  cmdHdl.addCmdDelim();
-  cmdHdl.addCmdString(COMMANDACCELSTEPPER_BONJOUR_ID);
-  cmdHdl.addCmdTerm();
-  cmdHdl.sendCmdSerial();
+    cmdHdl.initCmd();
+    cmdHdl.addCmdString(BONJOUR_CMD);
+    cmdHdl.addCmdDelim();
+    cmdHdl.addCmdString(COMMANDACCELSTEPPER_BONJOUR_ID);
+    cmdHdl.addCmdTerm();
+    cmdHdl.sendCmdSerial();
 }
-
 
 /**
-* unrecognized command and wrapper
+* Default unrecognized command and wrapper
+* Respond a message with header "?" and the command received as the argument
 */
-void CommandAccelStepper::wrapper_unrecognized(const char *command) {
-  // explicitly cast to a pointer to Classname
-  CommandAccelStepper* self = (CommandAccelStepper*) globalCommandAccelStepperPt2Object;
-  self->unrecognized(command);
+void CommandAccelStepper::wrapper_unrecognized(const char *command)
+{
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->unrecognized(command);
 }
 
-void CommandAccelStepper::unrecognized(const char *command) {
-  cmdHdl.initCmd();
-  cmdHdl.addCmdString("?");
-  cmdHdl.addCmdDelim();
-  cmdHdl.addCmdString(command);
-  cmdHdl.addCmdTerm();
-  cmdHdl.sendCmdSerial();
+void CommandAccelStepper::unrecognized(const char *command)
+{
+    cmdHdl.initCmd();
+    cmdHdl.addCmdString(UNRECOGNIZED_CMD);
+    cmdHdl.addCmdDelim();
+    cmdHdl.addCmdString(command);
+    cmdHdl.addCmdTerm();
+    cmdHdl.sendCmdSerial();
 }
 
+//Sets the current position
+void CommandAccelStepper::wrapper_setCurrentPosition()
+{
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->setCurrentPosition();
+}
 
-/**
-* move to command and wrapper
-*/
+void CommandAccelStepper::setCurrentPosition()
+{
+    long steps = cmdHdl.readLongArg();
+    if(cmdHdl.argOk)
+    {
+        linearactuator.setCurrentPosition(steps);
+    }
+}
+
+//Sets the speed of the stepper
+void CommandAccelStepper::wrapper_setSpeed()
+{
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->setSpeed();
+}
+
+void CommandAccelStepper::setSpeed()
+{
+    float stepsPerSec = cmdHdl.readFloatArg();
+    if(cmdHdl.argOk)
+    {
+        linearactuator.setSpeed(stepsPerSec);
+    }
+}
+
+//Gets the speed
+void CommandAccelStepper::wrapper_speed()
+{
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->speed();
+}
+
+float CommandAccelStepper::speed()
+{
+    cmdHdl.initCmd();
+    cmdHdl.addCmdString(COMMANDACCELSTEPPER_SPEED);
+    cmdHdl.addCmdDelim();
+    cmdHdl.addCmdFloat(linearactuator.speed());
+    cmdHdl.addCmdTerm();
+    cmdHdl.sendCmdSerial();
+}
+
+//Sets the max speed of the stepper
+void CommandAccelStepper::wrapper_setMaxSpeed()
+{
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->setMaxSpeed();
+}
+
+void CommandAccelStepper::setMaxSpeed()
+{
+    float stepsPerSec = cmdHdl.readFloatArg();
+    if(cmdHdl.argOk)
+    {
+        linearactuator.setMaxSpeed(stepsPerSec);
+    }
+}
+
+//Gets the max speed of the stepper
+void CommandAccelStepper::wrapper_maxSpeed()
+{
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->maxSpeed();
+}
+
+float CommandAccelStepper::maxSpeed()
+{
+    cmdHdl.initCmd();
+    cmdHdl.addCmdString(COMMANDACCELSTEPPER_MAXSPEED);
+    cmdHdl.addCmdDelim();
+    cmdHdl.addCmdFloat(linearactuator.maxSpeed());
+    cmdHdl.addCmdTerm();
+    cmdHdl.sendCmdSerial();
+}
+
+//Sets the acceleration of the stepper
+void CommandAccelStepper::wrapper_setAcceleration()
+{
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->setAcceleration();
+}
+
+void CommandAccelStepper::setAcceleration()
+{
+    float stepsPerSecPerSec = cmdHdl.readFloatArg();
+    if(cmdHdl.argOk)
+    {
+        linearactuator.setAcceleration(stepsPerSecPerSec);
+    }
+}
+
+//Gets the acceleration
+void CommandAccelStepper::wrapper_acceleration()
+{
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->acceleration();
+}
+
+float CommandAccelStepper::acceleration()
+{
+    cmdHdl.initCmd();
+    cmdHdl.addCmdString(COMMANDACCELSTEPPER_ACCELERATION);
+    cmdHdl.addCmdDelim();
+    cmdHdl.addCmdFloat(linearactuator.acceleration());
+    cmdHdl.addCmdTerm();
+    cmdHdl.sendCmdSerial();
+}
+
+//Enables the stepper acceleration
+void CommandAccelStepper::wrapper_enableAcceleration()
+{
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->enableAcceleration();
+}
+
+void CommandAccelStepper::enableAcceleration()
+{
+    linearactuator.enableAcceleration();
+}
+
+//Disables the stepper acceleration
+void CommandAccelStepper::wrapper_disableAcceleration()
+{
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->disableAcceleration();
+}
+
+void CommandAccelStepper::disableAcceleration()
+{
+    linearactuator.disableAcceleration();
+}
+
+//Moves the stepper to a location.
 void CommandAccelStepper::wrapper_moveTo()
 {
-  // explicitly cast to a pointer to Classname
-  CommandAccelStepper* self = (CommandAccelStepper*) globalCommandAccelStepperPt2Object;
-  self->moveTo();
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->moveTo();
 }
 
-void CommandAccelStepper::moveTo() {
-  #ifdef COMMANDACCELSTEPPER_DEBUG
-    Serial.println("Stepper received moveTo command");
-  #endif
+void CommandAccelStepper::moveTo()
+{
+    long steps = cmdHdl.readLongArg();
+    if(cmdHdl.argOk)
+    {
+        linearactuator.moveTo(steps);
+    }
+}
 
-  long nSteps = cmdHdl.readLongArg();
-  if (cmdHdl.argOk) {
-    #ifdef COMMANDACCELSTEPPER_DEBUG
-      Serial.print("nSteps: ");
-      Serial.println(nSteps);
-    #endif
-    stepper.moveTo(nSteps);
-  }
-  else {
-    #ifdef COMMANDACCELSTEPPER_DEBUG
-      Serial.println("No arguments");
-    #endif
-  }
+//Moves the stepper
+void CommandAccelStepper::wrapper_move()
+{
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->move();
+}
+
+void CommandAccelStepper::move()
+{
+    long steps = cmdHdl.readLongArg();
+    if(cmdHdl.argOk)
+    {
+        linearactuator.move(steps);
+    }
+}
+
+//Stops the movement of the stepper
+void CommandAccelStepper::wrapper_stop()
+{
+    CommandAccelStepper* self = (CommandAccelStepper*)globalCommandLinearAccelStepperActuatorPt2Object;
+    self->stop();
+}
+
+void CommandAccelStepper::stop()
+{
+    linearactuator.stop();
+}
+
+//Determines if the stepper is moving
+void CommandAccelStepper::wrapper_isMoving()
+{
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->isMoving();
+}
+
+void CommandAccelStepper::isMoving()
+{
+    cmdHdl.initCmd();
+    cmdHdl.addCmdString(COMMANDACCELSTEPPER_MOVING);
+    cmdHdl.addCmdDelim();
+    cmdHdl.addCmdBool(linearactuator.isMoving());
+    cmdHdl.addCmdTerm();
+    cmdHdl.sendCmdSerial();
+}
+
+//Determines the distance left to travel
+void CommandAccelStepper::wrapper_distanceToGo()
+{
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->distanceToGo();
+}
+
+void CommandAccelStepper::distanceToGo()
+{
+    cmdHdl.initCmd();
+    cmdHdl.addCmdString(COMMANDACCELSTEPPER_DIST);
+    cmdHdl.addCmdDelim();
+    cmdHdl.addCmdLong(linearactuator.distanceToGo());
+    cmdHdl.addCmdTerm();
+    cmdHdl.sendCmdSerial();
+}
+
+//Gets the target position
+void CommandAccelStepper::wrapper_targetPosition()
+{
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->targetPosition();
+}
+
+void CommandAccelStepper::targetPosition()
+{
+    cmdHdl.initCmd();
+    cmdHdl.addCmdString(COMMANDACCELSTEPPER_TARGET);
+    cmdHdl.addCmdDelim();
+    cmdHdl.addCmdLong(linearactuator.targetPosition());
+    cmdHdl.addCmdTerm();
+    cmdHdl.sendCmdSerial();
+}
+
+//Gets the current position
+void CommandAccelStepper::wrapper_currentPosition()
+{
+    CommandAccelStepper* self = (CommandAccelStepper*) globalCommandLinearAccelStepperActuatorPt2Object;
+    self->currentPosition();
+}
+
+void CommandAccelStepper::currentPosition()
+{
+    cmdHdl.initCmd();
+    cmdHdl.addCmdString(COMMANDACCELSTEPPER_POSITION);
+    cmdHdl.addCmdDelim();
+    cmdHdl.addCmdLong(linearactuator.currentPosition());
+    cmdHdl.addCmdTerm();
+    cmdHdl.sendCmdSerial();
 }
--- CommandAccelStepper/CommandAccelStepper.h
+++ CommandAccelStepper/CommandAccelStepper.h
@@ -2,35 +2,71 @@
 #define CommandAccelStepper_h
 
 #if defined(WIRING) && WIRING >= 100
-  #include <Wiring.h>
+    #include <Wiring.h>
 #elif defined(ARDUINO) && ARDUINO >= 100
-  #include <Arduino.h>
+    #include <Arduino.h>
 #else
-  #include <WProgram.h>
+    #include <WProgram.h>
 #endif
 
 #include <CommandHandler.h>
 #include <CommandManager.h>
 
-#include <AccelStepper.h>
+#include <LinearAccelStepperActuator.h>
 
-// the bonjour id of this device
-#define COMMANDACCELSTEPPER_BONJOUR_ID "ACCELSTEPPER"
+#define UNRECOGNIZED_CMD "?"
+#define BONJOUR_CMD "BONJOUR"
 
-// incoming
-#define COMMANDACCELSTEPPER_MOVE_TO "P"
+//Bonjour ID of the device
+#define COMMANDACCELSTEPPER_BONJOUR_ID "LINEARACCELSTEPPER"
 
-// Uncomment the next line to run the library in debug mode (verbose messages)
-// #define COMMANDACCELSTEPPER_DEBUG
+//Incoming commands
+#define COMMANDACCELSTEPPER_SET_POSITION "SP"
+#define COMMANDACCELSTEPPER_SET_SPEED "SS"
+#define COMMANDACCELSTEPPER_SET_MAX_SPEED "SMS"
+#define COMMANDACCELSTEPPER_SET_ACC "SA"
 
-class CommandAccelStepper {
-  public:
-    CommandAccelStepper(uint8_t interface = AccelStepper::FULL4WIRE, uint8_t pin1 = 2, uint8_t pin2 = 3, uint8_t pin3 = 4, uint8_t pin4 = 5, bool enable = true);
+#define COMMANDACCELSTEPPER_ENABLE_ACC "EA"
+#define COMMANDACCELSTEPPER_DISABLE_ACC "DA"
 
-    AccelStepper stepper;
+#define COMMANDACCELSTEPPER_MOVE_TO "MT"
+#define COMMANDACCELSTEPPER_MOVE "M"
+#define COMMANDACCELSTEPPER_STOP "S"
+
+#define COMMANDACCELSTEPPER_REQUEST_DIST "RD"
+#define COMMANDACCELSTEPPER_REQUEST_TARGET "RT"
+#define COMMANDACCELSTEPPER_REQUEST_POSITION "RP"
+
+#define COMMANDACCELSTEPPER_REQUEST_SPEED "RIS"
+#define COMMANDACCELSTEPPER_REQUEST_MAXSPEED "RIMS"
+#define COMMANDACCELSTEPPER_REQUEST_ACCELERATION "RIA"
+
+//outgoing command
+#define COMMANDACCELSTEPPER_MOVING "M"
+#define COMMANDACCELSTEPPER_DIST "D"
+#define COMMANDACCELSTEPPER_TARGET "T"
+#define COMMANDACCELSTEPPER_POSITION "P"
+
+#define COMMANDACCELSTEPPER_SPEED "IS"
+#define COMMANDACCELSTEPPER_MAXSPEED "IMS"
+#define COMMANDACCELSTEPPER_ACCELERATION "IA"
+
+
+//Uncomment the next line to run library in debug mode (verbose messages)
+// #define CommandAccelStepper_DEBUG
+
+class CommandAccelStepper
+{
+public:
+
     CommandHandler cmdHdl;
+    LinearAccelStepperActuator linearactuator;
 
-    void registerToCommandManager(CommandManager &cmdMng, const char *command);
+
+    CommandAccelStepper(AccelStepper &myStepper, int myEnablePin = -1);
+
+
+    void registerToCommandManager(CommandManager &cmdMgr, const char *command);
 
     static void wrapper_init(void* pt2Object);
     void init();
@@ -38,22 +74,66 @@ class CommandAccelStepper {
     static void wrapper_handleCommand(const char *command, void* pt2Object);
     void handleCommand(const char *command);
 
-    static void wrapper_setHeader(const char *command,  void* pt2Object);
+    static void wrapper_setHeader(const char *command, void* pt2Object);
     void setHeader(const char *command);
 
     static void wrapper_update(void* pt2Object);
     void update();
 
-  private:
+    static void wrapper_setCurrentPosition();
+    void setCurrentPosition();
 
+    static void wrapper_setSpeed();
+    void setSpeed();
+
+    static void wrapper_speed();
+    float speed();
+
+    static void wrapper_setMaxSpeed();
+    void setMaxSpeed();
+
+    static void wrapper_maxSpeed();
+    float maxSpeed();
+
+    static void wrapper_setAcceleration();
+    void setAcceleration();
+
+    static void wrapper_acceleration();
+    float acceleration();
+
+    static void wrapper_enableAcceleration();
+    void enableAcceleration();
+
+    static void wrapper_disableAcceleration();
+    void disableAcceleration();
+
+    static void wrapper_moveTo();
+    void moveTo();
+
+    static void wrapper_move();
+    void move();
+
+    static void wrapper_stop();
+    void stop();
+
+    static void wrapper_isMoving();
+    void isMoving();
+
+    static void wrapper_distanceToGo();
+    void distanceToGo();
+
+    static void wrapper_targetPosition();
+    void targetPosition();
+
+    static void wrapper_currentPosition();
+    void currentPosition();
+
+private:
     static void wrapper_bonjour();
     void bonjour();
 
     static void wrapper_unrecognized(const char *command);
     void unrecognized(const char *command);
-
-    static void wrapper_moveTo();
-    void moveTo();
 };
 
 #endif
